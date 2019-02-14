3d6c1229b27674c3c03e764e82b221cfff605bc6
Matt Stein <matt@workingconcept.com>
2019-02-14 14:52:27 -0800

    Try different approach.
    `trackPageview` not firing, probably because there isn’t a running process or .env to check.

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -4,16 +4,13 @@ export default ({
     router, // the router instance for the app
     siteData // site metadata
 }) => {
-    if (process.env.NODE_ENV === 'production' && typeof window !== 'undefined') {
-
+    if (process.env.NODE_ENV !== 'development' && typeof window !== 'undefined') {
         // Fathom snippet already injected by Netflify; just register pageViews
-        
         if (typeof fathom !== 'undefined') {
             router.afterEach(function(to) {
                 fathom('set', 'trackerUrl', to.fullPath)
                 fathom('trackPageview')
             });
         }
-
     }
 }
