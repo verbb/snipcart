fa1a3658994bfe8c7629e6c7ad7e6715fb8949fc
Matt Stein <matt@workingconcept.com>
2019-03-03 20:36:40 -0800

    Allow sku column to be null.
    Otherwise Section re-save can fail when a Product Details field is added to an existing field layout.

---
+++ src/migrations/m190304_034411_allow_null_sku.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace workingconcept\snipcart\migrations;
+
+use Craft;
+use craft\db\Migration;
+
+/**
+ * m190304_034411_allow_null_sku migration.
+ */
+class m190304_034411_allow_null_sku extends Migration
+{
+    // Public Properties
+    // =========================================================================
+
+    public $productDetailsTable = '{{%snipcart_product_details}}';
+
+
+    // Public Methods
+    // =========================================================================
+
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        if ($this->getDb()->tableExists($this->productDetailsTable))
+        {
+            /**
+             * Allow storing null in the column to keep from causing an Element
+             * re-save failure after adding a new Product Details field
+             * to a Section.
+             */
+            $this->alterColumn(
+                $this->productDetailsTable,
+                'sku',
+                $this->string()->null()
+            );
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        echo "m190304_034411_allow_null_sku cannot be reverted.\n";
+        return false;
+    }
+}
