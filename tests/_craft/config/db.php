df8d061fc75514e8c243e1c7705ebeaa21f92e29
Matt Stein <matt@workingconcept.com>
2020-09-07 15:04:30 -0500

    Update CI stuff.

--- tests/_craft/config/db.php
+++ tests/_craft/config/db.php
@@ -11,13 +11,9 @@
 use craft\helpers\App;
 
 return [
-    'dsn' => App::env('DB_DSN') ?: null,
-    'driver' => App::env('DB_DRIVER'),
-    'server' => App::env('DB_SERVER'),
-    'port' => App::env('DB_PORT'),
-    'database' => App::env('DB_DATABASE'),
-    'user' => App::env('DB_USER'),
-    'password' => App::env('DB_PASSWORD'),
-    'schema' => App::env('DB_SCHEMA'),
-    'tablePrefix' => App::env('DB_TABLE_PREFIX'),
+    'dsn' => getenv('DB_DSN'),
+    'user' => getenv('DB_USER'),
+    'password' => getenv('DB_PASSWORD'),
+    'schema' => getenv('DB_SCHEMA'),
+    'tablePrefix' => getenv('DB_TABLE_PREFIX'),
 ];
