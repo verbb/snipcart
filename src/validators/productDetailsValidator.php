e2a8f3176425785ada358abe9be5e5677948bc49
Pete Eveleigh <pete@fostercommerce.com>
2022-06-02 12:21:08 +0100

    add validation rule

--- src/validators/productDetailsValidator.php
+++ src/validators/productDetailsValidator.php
@@ -26,25 +26,38 @@ class ProductDetailsValidator extends Validator
     public function init()
     {
         parent::init();
-
-
     }
 
     /**
      * @inheritdoc
      * @throws InvalidConfigException
      */
-    public function validateAttribute($model, $attribute)
+    public function validateAttribute($model, $attribute): void
     {
         $value = $model->$attribute;
-   
-	if($value['sku'] === null || $value['sku'] ===''){
-		$this->addError($model, $attribute, 'SKU cannot be blank');
+		// test for empty SKU
+		if($value['sku'] === null || $value['sku'] ===''){
+			$this->addError($model, $attribute, 'SKU cannot be blank');
+		}
+		// test for empty inventory
+		if($value['inventory'] < 0){
+			$this->addError($model, $attribute, 'Inventory cannot be less than 0');
+		}
+		// test for non integer inventory
+		if(!is_int($value['inventory'])){
+			$this->addError($model, $attribute, 'Inventory must be a whole number');
+		}
+		// test for empty price
+		if($value['price'] === null || $value['price'] ===''){
+			$this->addError($model, $attribute, 'Price cannot be blank');
+		}
+		// test for negative price
+		if($value['price'] < 0){
+			$this->addError($model, $attribute, 'Price cannot be negative');
+		}
 	}
-       
-    }
 
-    public function isEmpty($value)
+    public function isEmpty($value): bool
     {
         if ($this->isEmpty !== null) {
             return parent::isEmpty($value);
@@ -52,4 +65,15 @@ class ProductDetailsValidator extends Validator
 
         return empty($value);
     }
+	
+	public function isNegative($value): bool
+	{
+		return $value < 0;
+	}
+	
+	public function isInteger($value): bool
+	{
+		return is_int($value);
+	}
 }
+
